e9c46def98d642482b4c9d83a5b4c1e895146524
Martin Hilton <mhilton@influxdata.com>
2022-12-01 09:11:14 +0000

    part 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io::stdin;
+use std::str::FromStr;
+
 fn main() {
-    println!("Hello, world!");
+    let mut count = 0;
+    let mut max = 0;
+    for line in stdin().lines() {
+        let s = line.expect("day01");
+        if !s.is_empty() {
+            count += isize::from_str(&s).expect("day01");
+        } else {
+            if count > max {
+                max = count
+            };
+            count = 0;
+        };
+    }
+
+    println!("{}", max);
 }
