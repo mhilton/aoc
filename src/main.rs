1786aaeff9c1939ff468d987a662876d446eb668
Martin Hilton <mhilton@influxdata.com>
2022-12-01 09:17:46 +0000

    part 2

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,17 @@ use std::str::FromStr;
 
 fn main() {
     let mut count = 0;
-    let mut max = 0;
+    let mut carrying = Vec::<isize>::new();
     for line in stdin().lines() {
         let s = line.expect("day01");
         if !s.is_empty() {
             count += isize::from_str(&s).expect("day01");
         } else {
-            if count > max {
-                max = count
-            };
+            carrying.push(count);
             count = 0;
         };
     }
-
-    println!("{}", max);
+    carrying.sort_unstable();
+    let sum: isize = (&carrying[carrying.len() - 3..]).into_iter().sum();
+    println!("{}", sum);
 }
