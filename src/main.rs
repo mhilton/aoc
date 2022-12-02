b59577cecbe588b82d7dd9979227819c0b5543fb
Martin Hilton <mhilton@influxdata.com>
2022-12-02 07:51:34 +0000

    part 2

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,27 @@
 use std::io::stdin;
 
+const LOSE: isize = 0;
+const DRAW: isize = 3;
+const WIN: isize = 6;
+
+const ROCK: isize = 1;
+const PAPER: isize = 2;
+const SCISSORS: isize = 3;
+
 fn main() {
     let score: isize = stdin()
         .lines()
         .map(|s| match s.unwrap().as_str() {
             // As there are only 9 valid strings precompute the outcomes.
-            "A X" => 1 + 3,
-            "A Y" => 2 + 6,
-            "A Z" => 3 + 0,
-            "B X" => 1 + 0,
-            "B Y" => 2 + 3,
-            "B Z" => 3 + 6,
-            "C X" => 1 + 6,
-            "C Y" => 2 + 0,
-            "C Z" => 3 + 3,
+            "A X" => LOSE + SCISSORS,
+            "A Y" => DRAW + ROCK,
+            "A Z" => WIN + PAPER,
+            "B X" => LOSE + ROCK,
+            "B Y" => DRAW + PAPER,
+            "B Z" => WIN + SCISSORS,
+            "C X" => LOSE + PAPER,
+            "C Y" => DRAW + SCISSORS,
+            "C Z" => WIN + ROCK,
             // Follow ed's UI and simply invite the user to reflect on their failure.
             _ => panic!("?"),
         })
