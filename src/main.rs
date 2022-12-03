0286d2a95f92b795875c84e12fa521cb2598759f
Martin Hilton <mhilton@influxdata.com>
2022-12-03 08:33:54 +0000

    part 1

--- src/main.rs
+++ src/main.rs
@@ -8,5 +8,16 @@ fn main() {
 }
 
 fn run(lines: impl Iterator<Item = String>) -> impl Display {
-    "!!TODO!!"
+    lines
+        .map(|s| {
+            let (a, b) = s.split_at(s.len() / 2);
+            let i = a.find(|c| b.contains(c)).unwrap();
+            a[i..].chars().take(1).next().unwrap()
+        })
+        .map(|c| match c {
+            c if 'a' <= c && c <= 'z' => 1 + u32::from(c) - u32::from('a'),
+            c if 'A' <= c && c <= 'Z' => 27 + u32::from(c) - u32::from('A'),
+            _ => panic!("?"),
+        })
+        .sum::<u32>()
 }
