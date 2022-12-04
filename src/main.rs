d7af9c216f72978471604516461f04201ba0a932
Martin Hilton <mhilton@influxdata.com>
2022-12-04 11:04:20 +0000

    part 1

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,38 @@
 // cargo run < ../input
 
+#[macro_use]
+extern crate lalrpop_util;
+
 use std::fmt::Display;
 use std::io::stdin;
+
+lalrpop_mod!(pub parse);
 
 fn main() {
     println!("{}", run(stdin().lines().map(|s| s.unwrap())));
 }
 
 fn run(lines: impl Iterator<Item = String>) -> impl Display {
-    "!!TODO!!"
+    lines
+        .map(|s| parse::PairParser::new().parse(&s).unwrap())
+        .map(|p| {
+            if p.0.envelops(p.1) || p.1.envelops(p.0) {
+                1
+            } else {
+                0
+            }
+        })
+        .sum::<usize>()
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct SectionRange {
+    start: isize,
+    end: isize,
+}
+
+impl SectionRange {
+    pub fn envelops(&self, r: SectionRange) -> bool {
+        self.start <= r.start && self.end >= r.end
+    }
 }
