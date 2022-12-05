1e3ea51b070d6054557820eeb45b92df9addb407
Martin Hilton <mhilton@influxdata.com>
2022-12-05 09:17:25 +0000

    part 1

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 // cargo run < ../input
 
+use std::collections::HashMap;
 use std::fmt::Display;
 use std::io::stdin;
 
@@ -8,5 +9,58 @@ fn main() {
 }
 
 fn run(lines: impl Iterator<Item = String>) -> impl Display {
-    "!!TODO!!"
+    let lines = lines.collect::<Vec<_>>();
+    let blank = lines.iter().position(|s| s.is_empty()).unwrap();
+    let (diag_lines, moves) = lines.split_at(blank);
+
+    let mut diag = diag_lines.iter().collect::<Vec<_>>();
+    diag.reverse();
+    let (label_lines, layout) = diag.split_at(1);
+    let labels = label_lines[0]
+        .split_whitespace()
+        .map(|s| s.to_string())
+        .collect::<Vec<_>>();
+
+    let mut stacks = HashMap::<String, Vec<char>>::new();
+    for level in layout {
+        for (i, c) in level
+            .chars()
+            .enumerate()
+            .filter(|&(i, c)| i % 4 == 1 && c != ' ')
+            .map(|(i, c)| (i / 4, c))
+        {
+            stacks
+                .entry(labels[i].clone())
+                .and_modify(|s| s.push(c))
+                .or_insert(vec![c]);
+        }
+    }
+
+    for line in moves {
+        if line.is_empty() {
+            continue;
+        }
+        let words = line
+            .split_whitespace()
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>();
+        let count: isize = words[1].parse().unwrap();
+        let mut temp = Vec::<char>::new();
+        stacks.entry(words[3].to_string()).and_modify(|v| {
+            for _ in 0..count {
+                temp.push(v.pop().unwrap())
+            }
+        });
+        temp.reverse();
+        stacks.entry(words[5].to_string()).and_modify(|v| {
+            for _ in 0..count {
+                v.push(temp.pop().unwrap())
+            }
+        });
+    }
+
+    labels
+        .into_iter()
+        .map(|s| stacks[&s].last().unwrap())
+        .collect::<String>()
 }
