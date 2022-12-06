2c536cf94997d9f606a9960e96bc3c0a3852393d
Martin Hilton <mhilton@influxdata.com>
2022-12-06 05:36:56 +0000

    part 1

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 // cargo run < ../input
 
-use std::fmt::Display;
-use std::io::stdin;
+use std::io::{stdin, Read};
 
 fn main() {
-    println!("{}", run(stdin().lines().map(|s| s.unwrap())));
-}
-
-fn run(lines: impl Iterator<Item = String>) -> impl Display {
-    "!!TODO!!"
+    let mut data = Vec::<u8>::new();
+    stdin().read_to_end(&mut data).unwrap();
+    for i in 0..data.len() {
+        let a = data[i];
+        let b = data[i + 1];
+        let c = data[i + 2];
+        let d = data[i + 3];
+        if a != b && a != c && a != d && b != c && b != d && c != d {
+            println!("{}", i + 4);
+            break;
+        }
+    }
 }
